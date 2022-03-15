4abc1b0b413f12aef1e26211fdfaa7db3e25b19a
kosyachniy <polozhev@mail.ru>
2022-03-16 01:33:51 +0300

    Don't display nested files

--- main.py
+++ main.py
@@ -2,8 +2,8 @@
 Find files from one directory that are not in another
 """
 
-
 from pathlib import Path
+from copy import deepcopy
 
 
 FOLDER_FROM = '/Users/kosyachniy/Desktop/TikTok'
@@ -17,18 +17,45 @@ def create_tree(folder):
         for file in Path(folder).iterdir()
     }
 
-def compare_tree(tree_from, tree_to):
+def compare_tree(tree_from, tree_to, short=True):
     """ Check for files of one file tree in another """
 
+    tree_from = deepcopy(tree_from)
+    tree_to = deepcopy(tree_to)
+
     for file in set(tree_from):
+        # Completely not in inherited
         if file not in tree_to:
+            # Don't display nested files
+            if short and tree_from[file]:
+                tree_from[file] = {}
+
             continue
 
-        if tree_from[file] and tree_to[file]:
-            tree_from[file] = compare_tree(tree_from[file], tree_to[file])
+        if tree_from[file] is None:
+            # If both are files, so remove it from tracking
+            if tree_to[file] is None:
+                del tree_from[file]
 
-        if not tree_from[file]:
-            del tree_from[file]
+            continue
+
+        # Both are there, it is necessary to check nested files
+        if tree_from[file] and tree_to[file]:
+            tree_new = compare_tree(tree_from[file], tree_to[file])
+
+            # All nested elements matched
+            if not tree_new:
+                del tree_from[file]
+                continue
+
+            # Don't display nested files
+            if short and {
+                k: v and {}
+                for k, v in tree_from[file].items()
+            } == tree_new:
+                tree_from[file] = {}
+            else:
+                tree_from[file] = tree_new
 
     return tree_from
 
--- tests/test_main.py
+++ tests/test_main.py
@@ -12,3 +12,9 @@ def test_more_files():
         create_tree('tests/data/nested_file/more'),
         create_tree('tests/data/nested_file/less')
     ) == {'onigiri': {'ramen': None, 'hacapuri': {}}, 'sake': None, 'sodzu': {}}
+
+def test_tree_pruning():
+    assert compare_tree(
+        create_tree('tests/data/dont_display/more'),
+        create_tree('tests/data/dont_display/less')
+    ) == {'hinkali': {}, 'sake': {}, 'ramen': {'onigiri': {'taiga': None}}}
